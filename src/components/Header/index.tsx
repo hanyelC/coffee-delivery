f9fe823fe018c40de9c0b10c2c718c41ff9b5f4a
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-02-16 11:30:10 -0300

    feat: style Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,26 @@
-import { HeaderContainer } from './styles'
+import { MapPin, ShoppingCart } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+import { Logo } from '../Logo'
+import { Address, Checkout, HeaderContainer } from './styles'
 
 export function Header() {
-  return <HeaderContainer>Header</HeaderContainer>
+  return (
+    <HeaderContainer>
+      <div>
+        <NavLink to="/">
+          <Logo />
+        </NavLink>
+        <nav>
+          <Address>
+            <MapPin weight="fill" size={22} /> Porto Alegre, RS
+          </Address>
+          <NavLink to="/checkout">
+            <Checkout>
+              <ShoppingCart weight="fill" size={22} />
+            </Checkout>
+          </NavLink>
+        </nav>
+      </div>
+    </HeaderContainer>
+  )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,51 @@
 import styled from 'styled-components'
 
-export const HeaderContainer = styled.div`
+export const HeaderContainer = styled.header`
+  background-color: ${({ theme }) => theme.colors.base.background};
   width: 100%;
+  padding: 3.2rem 0;
+
+  & > div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 112rem;
+    margin: 0 auto;
+
+    & > nav {
+      display: flex;
+      align-items: center;
+      gap: 1.2rem;
+    }
+  }
+`
+
+export const Checkout = styled.div`
+  background-color: ${({ theme }) => theme.colors.brand['primary-light']};
+  padding: 0.8rem;
+  border-radius: 6px;
+
+  & > svg {
+    fill: ${({ theme }) => theme.colors.brand['primary-dark']};
+  }
+`
+
+export const Address = styled.div`
+  background-color: ${({ theme }) => theme.colors.brand['secondary-light']};
+  color: ${({ theme }) => theme.colors.brand['secondary-dark']};
+  font-size: 1.4rem;
+  font-weight: 400;
+  line-height: 1.3;
+  font-family: ${({ theme }) => theme.fonts.text};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 4px;
+  padding: 1rem 0.8rem;
+  border-radius: 6px;
+
+  & > svg {
+    fill: ${({ theme }) => theme.colors.brand.secondary};
+  }
 `
